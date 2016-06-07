395bb8406b524345a1da2da887ed5da569c55bbc
Russell Ries <riesru@gmail.com>
2016-06-07 16:08:10 -0500

    add pin factory

---
+++ app/content/pins.factory.js
@@ -0,0 +1,9 @@
+angular.module("app")
+  .factory("pinFactory", (firebaseFactory) => {
+    let currentBoardId = null;
+
+    return {
+      setBoardId: boardId => currentBoardId = boardId,
+      getPins: () => firebaseFactory.getBoardPins(currentBoardId)
+    };
+  });
